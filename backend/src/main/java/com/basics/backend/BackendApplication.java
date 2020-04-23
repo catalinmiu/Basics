027f592c767009706f35b1409c773195f8cb706d
Catalin Miu <catalinmiu_98@yahoo.com>
2020-04-23 17:10:56 +0300

    add initial spring project

---
+++ backend/src/main/java/com/basics/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.basics.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
