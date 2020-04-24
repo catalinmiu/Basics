5b70ec9b2c6ff5db2b23b01d9724e1572d48d1e4
Catalin Miu <catalinmiu_98@yahoo.com>
2020-04-24 16:40:45 +0300

    added repository annotation to all repositories

--- backend/src/main/java/com/basics/backend/repository/RoleRepository.java
+++ backend/src/main/java/com/basics/backend/repository/RoleRepository.java
@@ -2,6 +2,8 @@ package com.basics.backend.repository;
 
 import com.basics.backend.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
