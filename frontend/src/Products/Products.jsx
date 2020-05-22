55bac685f3c20d86b47d7cdb7272e4b323d06c04
CoticiGabi <coticigeorgegabriel@yahoo.com>
2020-05-22 21:01:48 +0300

    added cards

--- frontend/src/Products/Products.jsx
+++ frontend/src/Products/Products.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import AppNav from "../AppNav";
+import { Card, CardDeck } from "react-bootstrap";
 class Products extends Component {
   state = {
     isLoading: true,
@@ -17,6 +18,59 @@ class Products extends Component {
     return (
       <div>
         <AppNav />
+        <div style={{ textAlign: "center" }} className="mb-4">
+          <h1>Products</h1>
+        </div>
+        <div className="container">
+          {Products.map((product) => (
+            <div>
+              <CardDeck>
+                <Card key={product.id}>
+                  <Card.Img variant="top" src="holder.js/100px160" />
+                  <Card.Body>
+                    <Card.Title>{product.title}</Card.Title>
+                    <Card.Text>{product.description}</Card.Text>
+                  </Card.Body>
+                  <Card.Footer>
+                    <small className="text-muted">{product.price}</small>
+                  </Card.Footer>
+                </Card>
+                <Card>
+                  <Card.Img variant="top" src="holder.js/100px160" />
+                  <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                      This card has supporting text below as a natural lead-in
+                      to additional content.{" "}
+                    </Card.Text>
+                  </Card.Body>
+                  <Card.Footer>
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
+                  </Card.Footer>
+                </Card>
+                <Card>
+                  <Card.Img variant="top" src="holder.js/100px160" />
+                  <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                      This is a wider card with supporting text below as a
+                      natural lead-in to additional content. This card has even
+                      longer content than the first to show that equal height
+                      action.
+                    </Card.Text>
+                  </Card.Body>
+                  <Card.Footer>
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
+                  </Card.Footer>
+                </Card>
+              </CardDeck>
+            </div>
+          ))}
+        </div>
         {Products.map((product) => (
           <div key={product.id}>
             <h3>{product.title}</h3>
